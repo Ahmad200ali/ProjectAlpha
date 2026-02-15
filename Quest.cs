c37cb0b541c3b72bfad84f8fdac695982ab0fdcb
Ahmad200ali <1116960@hr.nl>
2026-02-15 20:08:57 +0100

    working on quest filds

--- Quest.cs
+++ Quest.cs
@@ -3,17 +3,21 @@ public class Quest
     public int ID;
     public string? Name;
     public string? Description;
-    public bool isAlive;
+    public bool IsAlive;
    // public Monster monster;
-   public bool isCompleted;
+   public bool IsCompleted;
    public int KillCount = 0;
    public int FightLocationID;
 
-    public Quest(int id,string name,string description)
+    public Quest(int id,string name,string description,int fightLocationID , Monster monster)
     {
         ID =id;
         Name = name;
-        Description =description;   
+        Description = description;
+        FightLocationID = fightLocationID;
+        IsAlive = false;
+        KillCount = 0;
+           
     }
     public void StartQuest()
     {
