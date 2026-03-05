c396e3f549360bde4ddf4613976119d35d3fc0e4
Ahmad200ali <1116960@hr.nl>
2026-03-05 11:54:32 +0100

    Adding Weapon class

---
+++ Weapon.cs
@@ -0,0 +1,19 @@
+using System.Reflection.Metadata.Ecma335;
+
+public class Weapon
+{
+    public int Id;
+    public string Name;
+    public int MaximumDamage;
+
+
+    public Weapon(int id , string name , int maximumDamage)
+    {
+        Id= id;
+        Name =name;
+        MaximumDamage = maximumDamage;
+    }
+
+    
+    
+}
